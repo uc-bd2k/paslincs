b7bca09d23b401de13e15b38bc7ce4b9abfcfd8d
Yan Ren <renyn@ucmail.uc.edu>
2019-05-10 15:44:37 -0400

    updated CalcTPSig

--- R/CalcTPSig.R
+++ R/CalcTPSig.R
@@ -1,23 +1,25 @@
-CalcTPSig <- function(EdgeFrom,EdgeTo,interaction,AdjMat=NULL,ncomp=NULL,data,neigen=NULL) {
-  if (is.null(AdjMat)) {
-    AdjMat <- CalcAdj(EdgeFrom=EdgeFrom,EdgeTo=EdgeTo,interaction=interaction)
-  }
-  if (is.null(AdjMat)) {
-    print("Adjacency matrix is not available!")
-    return(NULL)
-  } else {
+CalcTPSig <- function(EdgeInfo,EdgeFrom,EdgeTo,pathway,interaction,ncomp=NULL,data,neigen=NULL) {
+  PathwayList <- unique(EdgeInfo[,pathway])
+  ScoreTab <- matrix(NA,ncol=ncol(data),nrow=length(PathwayList))
+  rownames(ScoreTab) <- PathwayList
+  for (path in PathwayList) {
+   Edge <- EdgeInfo[EdgeInfo$Pathway==path,]
+   AdjMat <- CalcAdj(EdgeFrom=Edge[,EdgeFrom],EdgeTo=Edge[,EdgeTo],interaction=Edge[,interaction])
+   if (is.null(AdjMat)) {
+    print(paste0("Adjacency matrix for ",path," is not available!"))
+   } else {
     Score <- CalcScore(AdjMat=AdjMat,ncomp=ncomp,data=t(data),neigen=neigen)
     if (length(Score)!=0) {
-      nonull <- sapply(Score,function(x) {!is.null(x)})
-      if (sum(nonull)>0) {
-        Score <- Score[nonull]
-        Score_summary <- rowSums(matrix(sapply(Score,function(x) as.numeric(x$Score)),ncol=length(Score)))
-        return(data.frame(Perturbagen=colnames(data),Score=Score_summary,stringsAsFactors=FALSE))
-      } else {
-        return(NULL)
-      }
-    } else {
-      return(NULL)
-    } 
+     nonull <- sapply(Score,function(x) {!is.null(x)})
+     if (sum(nonull)>0) {
+      Score <- Score[nonull]
+      Score_summary <- rowSums(matrix(sapply(Score,function(x) as.numeric(x$Score)),ncol=length(Score)))
+      ScoreTab[path,] <- Score_summary
+     }
+    }
+   }
   }
+  ScoreTab <- data.frame(ScoreTab,stringsAsFactors=FALSE)
+  colnames(ScoreTab) <- colnames(data)
+  return(ScoreTab)
 }
